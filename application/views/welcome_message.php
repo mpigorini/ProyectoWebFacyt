f3ea8cbda6b568cd74b03169225b93f4b7f54bfb
Genessis Jimenez <Genesis.jz.93@gmail.com>
2016-06-28 20:06:51 -0400

    Problema con el ng-view

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -83,7 +83,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
-
-<a href='#hola'>hola</a>
 </body>
 </html>
